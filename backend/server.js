25ed61c45e313599b573a4cf98961546b18e56f1
subika-04 <subikapro@gmail.com>
2026-01-23 11:15:50 +0530

    updated1

--- backend/server.js
+++ backend/server.js
@@ -7,26 +7,34 @@ const app = express();
 const mongoose=require("mongoose");
 // import dotenv (loads environment variables from .env file)
 const dotenv=require("dotenv");
+//import cors ()
+const cors=require("cors")
+
 
 //It makes the values written in .env available through process.env.
 dotenv.config()
+//Enables Cross-Origin Resource Sharing so that the backend can accept requests from different origins like frontend applications.
+app.use(cors())
 //converts request body into json object
 app.use(express.json())
+
 
 //Connect to database using connection link(MONGO_URI)
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{console.log("Mongodb connected successfully")})
 .catch((err)=>{console.log(err)});
 
-//Any request starting with /auth is forwarded to authRoutes
+// Handles all routes that start with /auth using authRoutes
 app.use('/auth',require('./routes/authRoutes'))
+// Handles all routes that start with /task using taskRoutes
 app.use('/task',require('./routes/taskRoutes'))
-//get request using /api
+
+// Handles GET request at /api
 app.get("/api", (req, res) => {
   res.send("hi from express");
 });
 
-//post request
+// Handles POST request at /api
 app.post('/api',(req,res)=>{
   const temp=req.body;
   res.send(temp);
